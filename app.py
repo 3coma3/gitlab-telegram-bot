72f4c2f0c029b53a6301385fe0d4097f50bc6e7e
3coma3 <3coma3@gmail.com>
2023-02-27 23:08:04 +0000

    refactor: message dictionary
    Store all formatted messages from the bot in a dictionary
    
    Rename msg_all() to broadcast()

--- app.py
+++ app.py
@@ -10,12 +10,49 @@ from formatters import eventFormatters as fmt
 from util import digest, dumpjson, new_secret, strange, tdif, timestamp as ts
 
 
+# formatted string from a dictionary
+def msg(*args):
+    return {
+        'online': "I'm online \U0001F44B",
+        'offline': "I'm going away for a while. Laters! \U0001F596",
+        'help': "Available commands:",
+        'ok': "Allright! \U0001F60E",
+        'cmd_unknown': "What? \U0001F633",
+        'cmd_private': "This is a PM-only command \U0001F610",
+        'sorry_owner': "I'm sorry Dave, I'm afraid I can't do that \U0001F916",
+        'arg_few': "I need more info \U0001F914",
+        'arg_extra': "I didn't understand this \"{0}\" \U0001F914",
+        'otp_list': "Here's the list of tokens:\n```\n{0}```\n",
+        'otp_new': "Ok! New token: {0}\n\ntype {1}\nexpires in {2}",
+        'otp_remove': "Ok! Deleted {0} token{1}",
+        'otp_flush': "Done! We have 0 tokens pending",
+        'otp_bad_type': "Can't use that token here",
+        'otp_bad_lifetime': "Wrong lifetime, must be between {0} and {1}",
+        'chg_list': "Here's the list of challenges:\n```\n{0}```\n",
+        'chg_new': "You have {0} to complete the challenge \U0001FE0F",
+        'chgp_remove': "Ok! Deleted {0} challenge{1}",
+        'chg_flush': "Done! We have 0 challenges pending",
+        'chg_unknown': "Hmm maybe forgot to /start? \U0001F9D0",
+        'chat_list': "Here's the list of chats:\n```\n{0}```\n",
+        'chat_auth': "\U0001F60E Ok, authorized!",
+        'chat_aauth': "\U0001F913 already authorized!",
+        'chat_deauth': "\U0001F60E Ok, deauthorized!",
+        'chat_leave': "I'll leave in {0} minutes if I'm not requested before.",
+        'chat_unauth': "\U0001F612 go away.",
+        'chat_quiet': "Going quiet now \U0001F910",
+        'chat_unknown': "I don't know that chat \U0001F914",
+        'owner_list': "Here's the list of bot owners:\n```\n{0}```\n",
+        'owner_remove': "Ok! {0} owner{1} gone",
+        'bot_auth': "\U0001F60E You're the boss!",
+        'bot_aauth': "Hi again boss \U0001F913"
+    }.get(args[0], args[0]).format(*args[1:])
+
+
 class GitlabBot(Bot):
     def __init__(self):
         self.configFile = 'config.json'
         super(GitlabBot, self).__init__()
 
-        self.send_to_all("I'm online \U0001F44B")
         self.owners = self.state.get('owners', [])
         self.chats = self.state.get('chats', [])
         self.otp = self.state.get('otp', [])
@@ -24,6 +61,14 @@ class GitlabBot(Bot):
         self.state['otp'] = self.otp
 
         self.challenges = []
+
+        self.broadcast(msg('online'))
+
+    def broadcast(self, m):
+        if m:
+            for c in self.chats:
+                if c['authorized'] and not c['quiet']:
+                    self.reply(c['id'], m)
 
     def user_entry(self, u):
         return {'id': u['id'],
@@ -111,10 +156,6 @@ class GitlabBot(Bot):
         else:
             self.reply(chatid, "\U0001F612 go away.")
 
-    def send_to_all(self, msg):
-        if msg:
-            for c in self.chats:
-                self.reply(c, msg)
 
 
 bot = GitlabBot()
@@ -133,17 +174,18 @@ def webhook():
             event = data[e]
             break
 
+    # TODO: move string to msg
     def nofmt():
         return 'New event "*{0}*" without formmater, write one for me!\n```\n{1}```\n'\
-               .format(event, json.dumps(data, indent=2))
+               .format(event, dumpjson(data))
 
-    bot.send_to_all(fmt.get(event, lambda _: nofmt())(data))
+    bot.broadcast(fmt.get(event, lambda _: nofmt())(data))
 
     return jsonify({'status': 'ok'})
 
 
 def exit():
-    bot.send_to_all("I'm going away for a while. Laters! \U0001F596")
+    bot.broadcast(msg('offline'))
 
 
 if __name__ == "__main__":
